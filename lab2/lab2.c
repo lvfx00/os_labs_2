bfbb88887c6fa334c0eb8c8d2d79efd6b1991f16
lvfx <lvfx00@gmail.com>
2018-09-26 03:20:23 +0000

    added 2nd lab

---
+++ lab2/lab2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+
+void *print_routine(void *arg) {
+        for(int i = 0; i < 10; ++i)
+                printf("Some text from child thread\n");
+
+        pthread_exit(NULL);
+}
+
+int main(int argc, char **argv) {
+        pthread_t th;
+
+        if (pthread_create(&th, NULL, print_routine, NULL) != 0) {
+                perror("pthread_create");
+                exit(EXIT_FAILURE);
+        }
+
+        if (pthread_join(th, NULL) != 0) {
+                perror("pthread_join");
+                exit(EXIT_FAILURE);
+        }
+
+        for(int i = 0; i < 10; ++i)
+                printf("Some text from parent thread\n");
+
+        pthread_exit(NULL);
+}
+
